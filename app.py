c4b4beb22ebe79094b5685990684687aa97d9966
Karen <ka.spk27@gmail.com>
2022-08-09 15:26:20 +0300

    inset

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        cur.execute("SELECT * FROM users WHERE username = %s;" %request.form.get("username"))
+        cur.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"),))
         rows = cur.fetchone()
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0][2], request.form.get("password")):
@@ -94,3 +94,83 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")        
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if checkUsername(username):
+            return apology("Invalid username", 403)
+        if  checkPassword(password):
+            return apology("Invalid password", 403)
+        if  password != confirmation:
+            return apology("Invalid confirmation", 403)
+        
+        # Проверка на существование пользователя
+        us = cur.execute("SELECT username FROM users WHERE username = ?", (username,)).fetchone()
+        if len(us) != 0:
+            return apology("User exist", 400)
+
+        # Добавляем пользователя и хеш пароля в бд
+        cur.execute("INSERT INTO users (username, hash) VALUES(?, ?)", (username, generate_password_hash(password, "pbkdf2:sha256"),))
+        conn.commit()
+        
+        # Remember which user has logged in
+        session["user_id"] = cur.execute("SELECT * FROM users WHERE username = ?", (username,))[0][0]
+        # Redirect user to home page
+        return redirect("/")
+    else:
+        return render_template("register.html")
+
+
+#function check password
+def checkPassword(passw):
+    symbols = ['!', '@', '#', '$', '%', '&', '?', '-', '+', '=', '~']
+    if passw or len(passw) > 6 or len(passw) < 30:
+        for s in symbols:
+            if s in passw:
+                for p in passw:
+                    if p.isdigit():
+                        for p in passw:
+                            if p.isupper():
+                                for p in passw:
+                                    if p.islower():
+                                        if checkPasswordBadSymbol(passw):
+                                            return False
+    return True
+
+def checkPasswordBadSymbol(passw):
+    symbols = ['!', '@', '#', '$', '%', '&', '?', '-', '+', '=', '~']
+    for p in passw:
+        if p not in symbols:
+            if not p.isdigit():
+                if not p.isupper():
+                    if not p.islower():
+                        return False
+    return True
+    
+# functionc check username
+def checkUsername(name):
+    if len(name) < 3 or len(name) > 30:
+            return True
+
+    symbols = ['@', '$', '&','-'];
+    for n in name:
+        if not n.isalpha():
+            if not n.isdigit():
+                if not n in symbols:
+                    return True
+    return False
