9213edc3643d5c04e71113fb522a0e72dcbc5d89
Karen <ka.spk27@gmail.com>
2022-08-09 12:45:59 +0300

    import

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask, json, request, flash, redirect, render_template, session
+import datetime
+from cs50 import SQL
+import sqlite3
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import apology, login_required, lookup, usd
+
 
 app = Flask(__name__) 
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route('/') 
 def index(): 
